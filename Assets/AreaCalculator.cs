64bff4084367e4cc87e8ab5e76083cd6827378f0
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-03-30 16:48:16 +0530

    Full working COde commented

--- Assets/AreaCalculator.cs
+++ Assets/AreaCalculator.cs
@@ -22,34 +22,34 @@ public class AreaCalculator : MonoBehaviour
 
         List<Vector3> vertices = new List<Vector3>()
         {
-           // new Vector3(0, 3, 0),
-            new Vector3(5, 5, 0),
+            new Vector3(0, 0, 0),
+            new Vector3(4, 5, 0),
             new Vector3(6, 0, 0),
-            new Vector3(0, 0, 0)
+           // new Vector3(0, 3, 0)
         };
         List<Vector3> normals = new List<Vector3>()
         {
-           // Vector3.forward,
+            //Vector3.forward,
             Vector3.forward,
             Vector3.forward,
             Vector3.forward
         };
 
-        List<int> triangles = new List<int>() { 2, 0, 1 };
+        List<int> triangles = new List<int>() { 0, 1, 2 };//, 0, 1, 3};
 
         mesh.Clear();
         mesh.SetVertices(vertices);
         mesh.SetNormals(normals);
         mesh.SetTriangles(triangles, 0);
 
-        Vector3 One_Three = vertices[0] - vertices[2];
-        Vector3 Two_Three = vertices[1] - vertices[2];
+/*        Vector3 vec_One = vertices[1] - vertices[0];
+        Vector3 vec_Two = vertices[2] - vertices[0];
 
-        float angle = Vector3.Angle(Two_Three, One_Three);
+        float angle = Vector3.Angle(vec_One, vec_Two);
         Debug.Log("Angle: " + angle);
-        float perperndicular_length = Vector3.Distance(vertices[2], vertices[0]) * Mathf.Sin(Mathf.Deg2Rad * angle);
-        float base_length = Vector3.Distance(vertices[2], vertices[1]);
+        float perperndicular_length = Vector3.Distance(vertices[1], vertices[0]) * Mathf.Sin(Mathf.Deg2Rad * angle);
+        float base_length = Vector3.Distance(vertices[2], vertices[0]);
         float area = 0.5f * base_length * perperndicular_length;
-        Debug.Log("base: " + base_length + " Perpendicular: " + perperndicular_length + " Area: "+""+area);
+        Debug.Log("base: " + base_length + " Perpendicular: " + perperndicular_length + " Area: "+""+area);*/
     }
 }
