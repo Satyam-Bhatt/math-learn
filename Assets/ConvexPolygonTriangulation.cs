5706a94d206aa61cd45fcebef2abd91f59ee523f
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-04-01 11:01:36 +0530

    Better Logic for triangulation

--- Assets/ConvexPolygonTriangulation.cs
+++ Assets/ConvexPolygonTriangulation.cs
@@ -12,37 +12,33 @@ public class ConvexPolygonTriangulation : MonoBehaviour
     [ContextMenu ("Triangulate")]
     public void Triangulate()
     {
-        if(nextVertex == null)
-        {
-            nextVertex = points[0];
-        }
-        Transform point = nextVertex;
+        Transform point = points[0];
         float distance_1 = 0;
         float distance_2 = 0;
         Transform vertex_1 = null;
         Transform vertex_2 = null;
 
-        foreach (Transform point2 in points)
+        foreach (Transform vert_1 in points)
         {
-            if (point != point2)
+            if(vert_1 != point)
             {
-                float distance = Vector3.Distance(point.position, point2.position);
-                if (distance_1 == 0 || distance < distance_1)
+                float distance = Vector3.Distance(point.position, vert_1.position);
+                if(distance_1 == 0 || distance < distance_1)
                 {
                     distance_1 = distance;
-                    vertex_1 = point2;
+                    vertex_1 = vert_1;
                 }
             }
         }
-        foreach (Transform point3 in points)
+        foreach (Transform vert_2 in points)
         {
-            if (point != point3 && point3 != vertex_1)
+            if(vert_2 != point && vert_2 != vertex_1)
             {
-                float distance = Vector3.Distance(point.position, point3.position);
-                if (distance_2 == 0 || distance < distance_2)
+                float distance = Vector3.Distance(point.position, vert_2.position);
+                if(distance_2 == 0 || distance < distance_2)
                 {
                     distance_2 = distance;
-                    vertex_2 = point3;
+                    vertex_2 = vert_2;
                 }
             }
         }
@@ -55,13 +51,35 @@ public class ConvexPolygonTriangulation : MonoBehaviour
         points.Remove(point);
         vertexToBecomePoints.Add(vertex_1);
         vertexToBecomePoints.Add(vertex_2);
-
-        if(points.Count > 2)
+        
+        while(points.Count > 2)
         {
-            nextVertex = vertexToBecomePoints[0];
+            distance_1 = 0;
+            point = vertexToBecomePoints[0];
             vertexToBecomePoints.RemoveAt(0);
-            //Triangulate();
+
+            foreach (Transform vert_1 in points)
+            {
+                if(vert_1 != point && vert_1 != vertexToBecomePoints[0])
+                {
+                    float distance = Vector3.Distance(point.position, vert_1.position);
+                    if(distance_1 == 0 || distance < distance_1)
+                    {
+                        distance_1 = distance;
+                        vertex_1 = vert_1;
+                    }
+                }
+            }
+
+            Debug.DrawLine(point.position, vertex_1.position, Color.red, 10f);
+            Debug.DrawLine(point.position, vertexToBecomePoints[0].position, Color.red, 10f);
+            Debug.DrawLine(vertex_1.position, vertexToBecomePoints[0].position, Color.red, 10f);
+
+            points.Remove(point);
+            vertexToBecomePoints.Add(vertex_1);
+
         }
+
     }
 
     // Start is called before the first frame update
