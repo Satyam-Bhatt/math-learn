7ce8ac75f1fde2293acc20af3e33c2f815cc82d6
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-04-10 11:14:36 +0530

    A bit more thought about the code

--- Assets/ConcaveMesh.cs
+++ Assets/ConcaveMesh.cs
@@ -7,6 +7,7 @@ public class ConcaveMesh : MonoBehaviour
     public Transform[] triangleVertices;
     public Transform outsidePoint;
 
+    private List<Transform> vertextToBecomePoints = new List<Transform>();
     float WedgeProduct(Vector3 a, Vector3 b)
     {
         return (a.x * b.y - a.y * b.x) / Mathf.Abs(a.x * b.y - a.y * b.x);
@@ -20,7 +21,7 @@ public class ConcaveMesh : MonoBehaviour
         return WedgeProduct(v1, v2);
     }
 
-    private void OnDrawGizmos()
+/*    private void OnDrawGizmos()
     {
         int side1 = Mathf.RoundToInt(PointCheck(triangleVertices[0].position, triangleVertices[1].position, outsidePoint.position));
         int side2 = Mathf.RoundToInt(PointCheck(triangleVertices[1].position, triangleVertices[2].position, outsidePoint.position));
@@ -31,6 +32,72 @@ public class ConcaveMesh : MonoBehaviour
         Gizmos.DrawLine(triangleVertices[0].position, triangleVertices[1].position);
         Gizmos.DrawLine(triangleVertices[1].position, triangleVertices[2].position);
         Gizmos.DrawLine(triangleVertices[2].position, triangleVertices[0].position);
+    }*/
+
+    private bool PointInsideOrOutside(Transform v1, Transform v2, Transform v3, Transform pt)
+    {
+        int side1 = Mathf.RoundToInt(PointCheck(v1.position, v2.position, pt.position));
+        int side2 = Mathf.RoundToInt(PointCheck(v2.position, v3.position, pt.position));
+        int side3 = Mathf.RoundToInt(PointCheck(v3.position, v1.position, pt.position));
+
+        if(side1 == side2 && side2 == side3)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    [ContextMenu ("Concave Mehs")]
+    public void Concave()
+    {
+        Transform point = triangleVertices[0];
+        float distance_1 = 0;
+        float distance_2 = 0;
+        Transform vertex_1 = null;
+        Transform vertex_2 = null;
+
+        foreach (Transform vert_1 in triangleVertices)
+        {
+            if(vert_1 != point)
+            {
+                float distance = Vector3.Distance(point.position, vert_1.position);
+                if(distance_1 == 0 || distance < distance_1)
+                {
+                    distance_1 = distance;
+                    vertex_1 = vert_1;
+                }
+            }
+        }
+        foreach (Transform vert_2 in triangleVertices)
+        {
+            if(vert_2 != point && vert_2 != vertex_1)
+            {
+                float distance = Vector3.Distance(point.position, vert_2.position);
+                if(distance_2 == 0 || distance < distance_2)
+                {
+                    distance_2 = distance;
+                    vertex_2 = vert_2;
+                }
+            }
+        }
+
+        foreach(Transform point_ in triangleVertices)
+        {
+            if(point_ != point && point_ != vertex_1 && point_ != vertex_2)
+            {
+                if(PointInsideOrOutside(vertex_1, vertex_2, point, point_))
+                {
+                    vertex_2 = point_;
+                }
+            }
+        }
+
+        Debug.DrawLine(point.position, vertex_1.position, Color.cyan , 20f);
+        Debug.DrawLine(point.position, vertex_2.position, Color.cyan, 20f);
+        Debug.DrawLine(vertex_1.position, vertex_2.position, Color.cyan, 20f);
     }
 
 }
