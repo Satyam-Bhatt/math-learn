efb3b318a6e0c73896a17ac8561cb627dcd5420f
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-04-16 12:23:30 +0530

    Render Circle Done

---
+++ Assets/CircleRenders.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CircleRenders : MonoBehaviour
+{
+    public float radius = 1f;
+    public float bigRadius = 2f;
+    public float anngle_ = 0f;
+
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.black;
+        Handles.DrawWireDisc(transform.position, Vector3.up, bigRadius);
+
+        Handles.color = Color.green;
+        Handles.DrawWireDisc(transform.position + (AngleToDirection(anngle_) * bigRadius), Vector3.up, radius);
+
+        Vector3 centerVector = AngleToDirection(anngle_) * bigRadius;
+
+        Vector3 sidePosition = (transform.position + (AngleToDirection(anngle_) * bigRadius) + AngleToDirection(90) * radius);
+        //sidePosition.x = bigRadius;
+        
+        Gizmos.DrawSphere(sidePosition, 0.1f);
+        
+    }
+
+    private Vector3 AngleToDirection(float angle)
+    {
+        return new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle) , 0, Mathf.Sin(Mathf.Deg2Rad * angle));
+    }
+}
