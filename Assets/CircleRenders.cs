9c50039455e10e2177f9eee07aaf75fe660c79b4
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-04-17 11:02:10 +0530

    Last question Almost Done

--- Assets/CircleRenders.cs
+++ Assets/CircleRenders.cs
@@ -8,6 +8,7 @@ public class CircleRenders : MonoBehaviour
     public float radius = 1f;
     public float radius2 = 1f;
     public float bigRadius = 2f;
+    public float displaceAngle = 0f;
     public float anngle_ = 0f;
 
     private void OnDrawGizmos()
@@ -16,7 +17,7 @@ public class CircleRenders : MonoBehaviour
         Handles.DrawWireDisc(transform.position, Vector3.up, bigRadius);
 
         Handles.color = Color.green;
-        Handles.DrawWireDisc(transform.position + (AngleToDirection(anngle_) * bigRadius), Vector3.up, radius);
+        Handles.DrawWireDisc(AngleToDirection(anngle_) * bigRadius, Vector3.up, radius);
         
         float angleProjected = Mathf.Asin(radius/(2*bigRadius)) * Mathf.Rad2Deg * 4;
         Vector3 locationEnd = AngleToDirection(angleProjected/2).normalized * bigRadius;
@@ -24,7 +25,19 @@ public class CircleRenders : MonoBehaviour
         //Gizmos.DrawSphere(locationEnd, 0.1f);
 
         float secondAngle = Mathf.Asin(radius2 / (2 * bigRadius)) * Mathf.Rad2Deg * 2;
-        Vector3 locationOfSecondCircle = (AngleToDirection(secondAngle + angleProjected / 2)) * bigRadius;
+        Vector3 locationOfSecondCircle = (AngleToDirection(secondAngle + angleProjected / 2 + displaceAngle)) * bigRadius;
+
+        float dist = Vector3.Distance(AngleToDirection(anngle_) * bigRadius, locationOfSecondCircle);
+        float radiusCombined = radius + radius2;
+        Debug.Log("Radius Combined: " + radiusCombined + " Distance: " + dist);
+        if(radius + radius2 > dist && radius + radius2 < dist + 0.1f)
+        {
+            displaceAngle += 0.1f;
+        }
+        else if(dist + 0.01f > radius + radius2)
+        {
+            displaceAngle -= 0.1f;
+        }
 
         Handles.DrawWireDisc(locationOfSecondCircle, Vector3.up, radius2);
     }
