c01d3cf474005698a26c75f1a510e5c3a67c1856
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-04-20 17:33:15 +0530

    All questions done

--- Assets/CircleRenders.cs
+++ Assets/CircleRenders.cs
@@ -5,41 +5,55 @@ using UnityEngine;
 
 public class CircleRenders : MonoBehaviour
 {
-    public float radius = 1f;
-    public float radius2 = 1f;
+/*    public float radius = 1f;
+    public float radius2 = 1f;*/
     public float bigRadius = 2f;
-    public float displaceAngle = 0f;
-    public float anngle_ = 0f;
+   // public float displaceAngle = 0f;
+    //public float anngle_ = 0f;
+
+    public 
+        List<float> radiuses = new List<float>();
 
     private void OnDrawGizmos()
     {
         Handles.color = Color.black;
         Handles.DrawWireDisc(transform.position, Vector3.up, bigRadius);
-
+        
         Handles.color = Color.green;
-        Handles.DrawWireDisc(AngleToDirection(anngle_) * bigRadius, Vector3.up, radius);
-        
-        float angleProjected = Mathf.Asin(radius/(2*bigRadius)) * Mathf.Rad2Deg * 4;
-        Vector3 locationEnd = AngleToDirection(angleProjected/2).normalized * bigRadius;
-        
-        //Gizmos.DrawSphere(locationEnd, 0.1f);
-
-        float secondAngle = Mathf.Asin(radius2 / (2 * bigRadius)) * Mathf.Rad2Deg * 2;
-        Vector3 locationOfSecondCircle = (AngleToDirection(secondAngle + angleProjected / 2 + displaceAngle)) * bigRadius;
-
-        float dist = Vector3.Distance(AngleToDirection(anngle_) * bigRadius, locationOfSecondCircle);
-        float radiusCombined = radius + radius2;
-        Debug.Log("Radius Combined: " + radiusCombined + " Distance: " + dist);
-        if(radius + radius2 > dist && radius + radius2 < dist + 0.1f)
+        Handles.DrawWireDisc(AngleToDirection(0) * bigRadius, Vector3.up, radiuses[0]);
+        float firstAngle = Mathf.Asin(radiuses[0] / (2 * bigRadius)) * Mathf.Rad2Deg * 2;
+        float anngle_ = 0f;
+        for (int i = 1; i < radiuses.Count; i++)
         {
-            displaceAngle += 0.1f;
-        }
-        else if(dist + 0.01f > radius + radius2)
-        {
-            displaceAngle -= 0.1f;
+            float secondAngle = Mathf.Asin(radiuses[i] / (2 * bigRadius)) * Mathf.Rad2Deg * 2;
+
+            Vector3 locationOfSecondCircle = (AngleToDirection(anngle_+ secondAngle + firstAngle)) * bigRadius;
+            Vector3 locationOfFirstCircle = (AngleToDirection(anngle_)) * bigRadius;
+
+            float dist = Vector3.Distance(locationOfFirstCircle, locationOfSecondCircle);
+
+            Vector3 r1Tor2 = locationOfSecondCircle - locationOfFirstCircle;
+            Vector3 centerTOr1 = transform.position - locationOfFirstCircle;
+
+            float phi = Vector3.Angle(r1Tor2, centerTOr1);
+
+            float tanAlpha1 = Mathf.Sin(phi * Mathf.Deg2Rad) / ((bigRadius / (dist - radiuses[i]) - Mathf.Cos(phi * Mathf.Deg2Rad)));
+            float tanAlpha2 = Mathf.Sin(phi * Mathf.Deg2Rad) / ((bigRadius / (dist - radiuses[i - 1]) - Mathf.Cos(phi * Mathf.Deg2Rad)));
+
+            float tanAlpha1_Value = Mathf.Atan(tanAlpha1) * Mathf.Rad2Deg;
+            float tanAlpha2_Value = Mathf.Atan(tanAlpha2) * Mathf.Rad2Deg;
+
+            float theta = 180 - 2 * phi - tanAlpha1_Value - tanAlpha2_Value;
+
+            locationOfSecondCircle = (AngleToDirection(anngle_ + secondAngle + firstAngle + theta)) * bigRadius;
+
+            Handles.DrawWireDisc(locationOfSecondCircle, Vector3.up, radiuses[i]);
+
+            anngle_ = anngle_ + firstAngle + secondAngle + theta;
+            firstAngle = secondAngle;
         }
 
-        Handles.DrawWireDisc(locationOfSecondCircle, Vector3.up, radius2);
+
     }
 
     private Vector3 AngleToDirection(float angle)
