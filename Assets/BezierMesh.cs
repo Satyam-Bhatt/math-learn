bd58554e5ff03563eb91771100e39c41459dd19b
Satyam-Bhatt <satyambhatt20@gmail.com>
2024-05-04 17:22:53 +0530

    Rendering bost sides with correct normals done

--- Assets/BezierMesh.cs
+++ Assets/BezierMesh.cs
@@ -32,7 +32,8 @@ public class BezierMesh : MonoBehaviour
         Gizmos.DrawSphere(l123_, 0.1f);
         Gizmos.color = Color.green;
         Gizmos.DrawRay(l123_, tangent);
-        Gizmos.DrawRay(l123_, new Vector3(-tangent.y,tangent.x,0));
+        Gizmos.DrawRay(l123_, new Vector3(-tangent.y,tangent.x,0)); //Up
+        Gizmos.DrawRay(l123_, new Vector3(tangent.y, -tangent.x, 0)); //Down
 
         int detail = 32;
         Vector3 lastPosition = points[0].position;
@@ -113,6 +114,28 @@ public class BezierMesh : MonoBehaviour
 
             Vector3 tangent = (l23-l12).normalized;
             desiNormal.Add(new Vector3(-tangent.y, tangent.x, 0));
+            desiNormal.Add(new Vector3(-tangent.y, tangent.x, 0));
+
+            lastPosition = l123;
+        }
+
+        for (int i = 0; i <= detail - 1; i++)
+        {
+            t = i / (float)detail;
+
+            Vector3 l1 = Vector3.Lerp(points[0].position, points[1].position, t);
+            Vector3 l2 = Vector3.Lerp(points[1].position, points[2].position, t);
+            Vector3 l3 = Vector3.Lerp(points[2].position, points[3].position, t);
+
+            Vector3 l12 = Vector3.Lerp(l1, l2, t);
+            Vector3 l23 = Vector3.Lerp(l2, l3, t);
+
+            Vector3 l123 = Vector3.Lerp(l12, l23, t);
+            middlePoints.Add(l123);
+
+            Vector3 tangent = (l23 - l12).normalized;
+            desiNormal.Add(new Vector3(tangent.y, -tangent.x, 0));
+            desiNormal.Add(new Vector3(tangent.y, -tangent.x, 0));
 
             lastPosition = l123;
         }
@@ -124,12 +147,12 @@ public class BezierMesh : MonoBehaviour
             vertices.Add(point - 3 * Vector3.forward);
         }
 
-        Debug.Log(vertices.Count);
+        Debug.Log("Verticies Count: "+vertices.Count + " Normal Count" + desiNormal.Count);
 
         List<int> triangles = new List<int>();
         List<Vector3> normals = new List<Vector3>();
         int addfactor = 0;
-        for(int i = 0; i <= vertices.Count/2 - 2; i = i + 1)
+        for(int i = 0; i <= vertices.Count/4 - 2; i = i + 1)
         {
             triangles.Add(0 + addfactor);
             triangles.Add(2 + addfactor);
@@ -142,8 +165,8 @@ public class BezierMesh : MonoBehaviour
             addfactor = addfactor + 2;
         }
 
-/*        addfactor = 0;
-        for (int i = 0; i <= vertices.Count / 2 - 2; i = i + 1)
+        addfactor = 0;
+        for (int i = 0; i <= vertices.Count / 4 - 2; i = i + 1)
         {
             triangles.Add(0 + addfactor);
             triangles.Add(1 + addfactor);
@@ -153,11 +176,8 @@ public class BezierMesh : MonoBehaviour
             triangles.Add(3 + addfactor);
             triangles.Add(2 + addfactor);
 
-            normals.Add(Vector3.down);
-            normals.Add(Vector3.down);
-
             addfactor = addfactor + 2;
-        }*/
+        }
 
         Debug.Log("Triangles" + triangles.Count);
 
